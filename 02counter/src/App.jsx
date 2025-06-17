339325c12e0e73acd3ae8701d640169d79ce8424
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-06-17 13:27:08 +0530

    counter proj

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,11 +1,35 @@
-import { useState } from 'react'
-import './App.css'
+import {useState} from 'react';
 
 function App() {
+  let [counter, setCounter] = useState(0)
 
+
+
+  const addValue = () => {
+    counter = counter + 1;
+    setCounter(counter)
+    console.log(counter);
+    
+  }
+
+  const subtractValue = () => {
+    counter = counter - 1;
+    setCounter(counter)
+  } 
+
+
+  
+    
+  
   return (
     <>
-      <h1>yoo</h1>
+      <h1>Counter</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br></br>
+      <button onClick={subtractValue}>Subtract value</button>
+
     </>
   )
 }
